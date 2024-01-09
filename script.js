0d54ae933a4ff4872bf7e4f40ba0299a50d18f01
Anish Neupane <anishnep51@gmail.com>
2024-01-10 00:10:41 +0545

    Optimized a bit for smaller screens

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-function checkMode(){
+function checkMode() {
     let mode = localStorage.getItem("mode");
     if (mode == "light") {
         changeMode("lightButton");
-    }else{
-        changeMode("darkButton")
+    } else {
+        changeMode("darkButton");
     }
 }
 
-function changeMode(button){
+function changeMode(button) {
     const darkButton = "darkButton";
     const lightButton = "lightButton";
     const body = document.getElementsByTagName("body")[0];
     const navList = document.querySelector(".nav-list");
     if (button == lightButton) {
-        body.style = `background: url('lightBG.jpg'); 
+        body.style = `background: url('images/lightBG.jpg'); 
         background-size: cover;
-        background-repeat: no-repeat;background-position: center;`
+        background-repeat: no-repeat;background-position: center;`;
         localStorage.setItem("mode", "light");
-    }else{
-        body.style = `background: url('darkBG.jpg'); 
+    } else {
+        body.style = `background: url('images/darkBG.jpg'); 
         background-size: cover;
-        background-repeat: no-repeat;background-position: center;`
+        background-repeat: no-repeat;background-position: center;`;
         localStorage.setItem("mode", "dark");
     }
-    console.log(localStorage.getItem("mode"));
+}
+
+function displayMenu(clickedId) {
+    let menu = document.getElementById("mobile-menu");
+    if (menu.style.display != "flex") {
+        menu.style = "display: flex;";
+    } else {
+        menu.style = "display: none;";
+    }
 }
