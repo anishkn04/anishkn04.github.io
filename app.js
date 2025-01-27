936b6c6a8e425ce53fc9707260dba0bb80219012
Anish K. Neupane <anishnep51@gmail.com>
2025-01-27 23:37:52 +0545

    feat: Adds redirection link to NCIT_Resources

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
+            if (button.classList.contains("drive")) {
+                window.open("https://drive.google.com/drive/folders/1pe-94T_oa9rZ19hPh2lLJpLFyDBjbG9I", "_blank")
+                return
+            }
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
             document.querySelector(".active").classList.remove("active");
